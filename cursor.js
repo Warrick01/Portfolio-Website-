1c8943f0e6dcbfe2c21fd7ab592e439d6ec23572
Warrick01 <jtwarric@asu.edu>
2021-11-07 17:10:39 -0700

    init

---
+++ cursor.js
@@ -0,0 +1,12 @@
+var showCursor = false
+var speed = 500
+
+setInterval(() => {
+if(!showCursor) {
+    document.getElementById('cursor').sty
+    showCursor = true
+}else {
+    document.getElementById('cursor').sty
+    showCursor = false
+}
+}, speed)
