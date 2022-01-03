25800120c36155a17de565275faa4d1e042ad49e
Warrick01 <jtwarric@asu.edu>
2022-01-03 12:40:41 -0700

    website update

--- cursor.js
+++ cursor.js
@@ -1,12 +1,12 @@
-var showCursor = false
-var speed = 500
+var cursor = true;
+var speed = 500;
 
 setInterval(() => {
-if(!showCursor) {
-    document.getElementById('cursor').sty
-    showCursor = true
-}else {
-    document.getElementById('cursor').sty
-    showCursor = false
-}
+    if(cursor) {
+        document.getElementById('cursor').style.opacity = 0;
+        cursor = false;
+    }else {
+        document.getElementById('cursor').style.opacity = 1;
+        cursor = true;
+    }
 }, speed)
--- projects.js
+++ projects.js
@@ -1,42 +1,40 @@
 const GITHUB_USERNAME = "Warrick01"
 
 fetch(`https://api.github.com/users/${GITHUB_USERNAME}/repos`)
-
 .then(response => response.json())
 .then(repos => {
 
-    for(const repo of repos)
+    for(const repo of repos) {
 
         const repoName = repo.name
         const repoDescription = repo.description
         const repoURL = repo.html_url
         const repoLanguage = repo.language
 
-        const cardContainer = document.createElement('div')
+        const cardContainer = document.createElement("div")
         cardContainer.className = "card center"
 
-        const cardImage = document.createElement('img')
-        cardImage.src = "./assets/generic_code_bgrnd.jpg"
-        cardImage.className = "project-image"
+        const cardImg = document.createElement("img")
+        cardImg.src = "./assets/generic_code_bgrnd.jpg"
+        cardImg.className = "project-image"
 
-        cardContainer.appendChild(cardImage)
+        cardContainer.appendChild(cardImg)
 
-        
-        const projectInfoContainer = document.createElement('div')
+        const projectInfoContainer = document.createElement("div")
         projectInfoContainer.className = "project-info-container"
 
-        const projectTitle = document.createElement('h3')
-        projectTitle.texContent = repoName
+        const projectTitle = document.createElement("h3")
+        projectTitle.textContent = repoName
 
-        const projectDescription = document.createElement('p')
+        const projectDescription = document.createElement("p")
         projectDescription.textContent = repoDescription
 
-        const projectURL = document.createElement('a')
+        const projectURL = document.createElement("a")
         projectURL.href = repoURL
-        projectURL.target = "_blank"
         projectURL.appendChild(projectTitle)
+        projectURL.target = "_blank"
 
-        const projectLanguage = document.createElement('p')
+        const projectLanguage = document.createElement("p")
         projectLanguage.textContent = repoLanguage
 
         projectInfoContainer.appendChild(projectURL)
@@ -45,10 +43,9 @@ fetch(`https://api.github.com/users/${GITHUB_USERNAME}/repos`)
 
         cardContainer.appendChild(projectInfoContainer)
 
-        const projetContainer = document.getElementById('projects_container')
+        const projectsContainer = document.getElementById("projects_container")
 
         projectsContainer.appendChild(cardContainer)
     }
 
-
-}) // check the repo from discord 
+});
